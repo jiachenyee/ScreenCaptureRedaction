40ba70ee92e09433fa58756f3b7d0dc7c593de0e
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-09-19 14:55:59 +0800

    initial implementation of package

--- Sources/ScreenCaptureRedaction/ScreenCaptureRedaction.swift
+++ Sources/ScreenCaptureRedaction/ScreenCaptureRedaction.swift
@@ -1,2 +1,75 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+struct RedactWhenScreenRecordedModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        ScreenCaptureRedactionView {
+            content
+        }
+    }
+}
+
+public extension View {
+    func redactWhenScreenRecorded() -> some View {
+        self.modifier(RedactWhenScreenRecordedModifier())
+    }
+}
+
+public struct ScreenCaptureRedactionView<Content: View>: View {
+    @ViewBuilder
+    public var content: (() -> Content)
+    
+    public var body: some View {
+        if #available(iOS 17.0, *) {
+            ScreenCaptureRedactionViewNew {
+                content()
+            }
+        } else {
+            ScreenCaptureRedactionViewOld {
+                content()
+            }
+        }
+    }
+}
+
+@available(iOS 17.0, *)
+struct ScreenCaptureRedactionViewNew<Content: View>: View {
+    
+    @Environment(\.isSceneCaptured) var isSceneCaptured
+    @Environment(\.scenePhase) var scenePhase
+    
+    @ViewBuilder
+    var content: (() -> Content)
+    
+    var body: some View {
+        if isSceneCaptured || scenePhase != .active {
+            content()
+                .redacted(reason: .placeholder)
+        } else {
+            content()
+        }
+    }
+}
+
+@available(iOS, obsoleted: 17.0, message: "Use ScreenCaptureRedactionViewNew instead.")
+struct ScreenCaptureRedactionViewOld<Content: View>: View {
+    
+    @State private var isSceneCaptured = UIScreen.main.isCaptured
+    @Environment(\.scenePhase) var scenePhase
+    
+    @ViewBuilder
+    var content: (() -> Content)
+    
+    var body: some View {
+        Group {
+            if isSceneCaptured || scenePhase != .active {
+                content()
+                    .redacted(reason: .placeholder)
+            } else {
+                content()
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIScreen.capturedDidChangeNotification)) { _ in
+            isSceneCaptured = UIScreen.main.isCaptured
+        }
+    }
+}
